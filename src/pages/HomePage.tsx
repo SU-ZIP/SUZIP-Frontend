8997db7bf48989d1262f3a088446b7cf4117a806
daahyunk <ekgusdl0408@naver.com>
2024-03-08 14:44:49 +0900

    [Feat] 홈 작업 중

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,55 @@
-import React from "react";
+import React from 'react';
+import styled from "styled-components";
 
-export default function HomePage() {
-  return <div>HomePage</div>;
+// 기존 Title 컴포넌트
+
+const CalendarContainer = styled.div`
+    margin-left: 10rem;
+`;
+
+const Title = styled.h1`
+    font-family: 'PPMonumentExtended', sans-serif;
+`;
+
+// 새로운 DayHeader 컴포넌트
+const DayHeader = styled.th`
+    font-family: 'PPMonumentExtended';
+    font-weight: medium;
+`;
+
+const HomePage: React.FC = () => {
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+                        "July", "August", "September", "October", "November", "December"];
+    const currentDate = new Date();
+    const currentMonth = monthNames[currentDate.getMonth()];
+
+    const renderCalendar = () => {
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <DayHeader>/Sun</DayHeader>
+                        <DayHeader>/Mon</DayHeader>
+                        <DayHeader>/Tue</DayHeader>
+                        <DayHeader>/Wed</DayHeader>
+                        <DayHeader>/Thu</DayHeader>
+                        <DayHeader>/Fri</DayHeader>
+                        <DayHeader>/Sat</DayHeader>
+                    </tr>
+                </thead>
+                <tbody>
+                    {/* 캘린더 날짜 셀을 여기에 추가합니다. */}
+                </tbody>
+            </table>
+        );
+    };
+
+    return (
+        <CalendarContainer>
+            <Title>{currentMonth}</Title> {/* 현재 월을 제목으로 표시 */}
+            {renderCalendar()} {/* 캘린더를 렌더링 */}
+        </CalendarContainer>
+    );
 }
+
+export default HomePage;
